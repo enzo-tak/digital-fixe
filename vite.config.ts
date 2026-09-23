3af23940b1ce8be6afffa41adb4748d6e3f7fe72
Enzo Takanashi <japoreba14l@gmail.com>
2026-09-23 19:06:36 +0100

    Add publicDir configuration

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig(({ mode }) => {
   const emitSourcemaps = mode === 'development'
 
   return {
+    publicDir: 'public',
     base: process.env.FIGMA_PUBLIC_URL ? `${process.env.FIGMA_PUBLIC_URL}/` : '/',
     build: {
       sourcemap: emitSourcemaps ? 'inline' : false,
